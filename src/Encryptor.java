16d377a2d9916b64ab36a4768654d309a55e0602
Alex Hadi <alexhisb@gmail.com>
2018-04-24 21:41:41 -0700

    Encryption now works.

---
+++ src/Encryptor.java
@@ -0,0 +1,134 @@
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.*;
+
+/**
+ * Class: Encryptor
+ * Allows messages to be encrypted and decrypted.
+ *
+ * @author Alex Hadi
+ * @version April 24, 2018
+ */
+public class Encryptor {
+    // This client's specific public and private keys.
+    private PublicKey publicKey;
+    private PrivateKey privateKey;
+
+    // Constant for encryption and decryption cipher.
+    private static final String CIPHER = "RSA/ECB/PKCS1Padding";
+
+    /**
+     * Constructor: Encryptor
+     * Class is initialized with IP & port. PublicKey & PrivateKey are set.
+     */
+    public Encryptor() {
+        // KeyPair initialized. Both PublicKey and PrivateKey are retrieved.
+        KeyPair keyPair = getKeyPair();
+        if (keyPair == null) {
+            System.out.println("Could not get the KeyPair!");
+            return;
+        }
+        this.privateKey = keyPair.getPrivate();
+        this.publicKey = keyPair.getPublic();
+    }
+
+    /**
+     * Method: getKeyPair
+     * Helper method to retrieve a KeyPair object. Uses RSA & SHA1PRNG.
+     *
+     * @return The KeyPair object that contains the PrivateKey and PublicKey.
+     */
+    private KeyPair getKeyPair() {
+        KeyPairGenerator keyPairGenerator;
+        SecureRandom secureRandom;
+
+        try {
+            keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            secureRandom = SecureRandom.getInstance("SHA1PRNG");
+        }
+        catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+
+        // 1024 bits set as the key size.
+        keyPairGenerator.initialize(1024, secureRandom);
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    /**
+     * Method: getDecryptedMessage
+     * Retrieves encrypted encryption from the server, decrypts it, and returns as a String.
+     *
+     * @return The encryption as a String.
+     */
+    public String getDecryptedMessage(byte[] encryptedMessage) {
+        Cipher cipher;
+        try {
+            cipher = Cipher.getInstance(CIPHER);
+        }
+        catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            return null;
+        }
+
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, this.privateKey);
+        }
+        catch (InvalidKeyException e) {
+            return null;
+        }
+
+        byte[] decryptedMessage;
+        try {
+            decryptedMessage = cipher.doFinal(encryptedMessage);
+        }
+        catch (IllegalBlockSizeException | BadPaddingException e) {
+            return null;
+        }
+
+        // To convert the byte[] to a String
+        return new String(decryptedMessage);
+    }
+
+    /**
+     * Method: encryptString
+     * Private helper method that encrypts a string and returns it as a byte array.
+     *
+     * @param message The String that represents the encryption.
+     * @return The encrypted string as a byte[].
+     */
+    public byte[] encryptString(String message, PublicKey encryptionKey) {
+        byte[] messageBytes = message.getBytes();
+
+        Cipher cipher;
+        try {
+            cipher = Cipher.getInstance(CIPHER);
+        }
+        catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            return null;
+        }
+
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, encryptionKey);
+        }
+        catch (InvalidKeyException e) {
+            return null;
+        }
+        
+        try {
+            return cipher.doFinal(messageBytes);
+        }
+        catch (IllegalBlockSizeException | BadPaddingException e) {
+            return null;
+        }
+    }
+
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public PrivateKey getPrivateKey() {
+        return privateKey;
+    }
+}
--- src/Server.java
+++ src/Server.java
@@ -7,8 +7,9 @@ import java.io.*;
 import java.net.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import javax.swing.*;
-
 
 public class Server extends JFrame {
     //Variable Declaration
@@ -21,9 +22,19 @@ public class Server extends JFrame {
     private String name;
     private String clientName;
 
+    private PublicKey myPublicKey;
+    private PrivateKey myPrivateKey;
+    private Encryptor myEncryptor = new Encryptor();
+
+    private PublicKey clientKey;
+
     //Server Constructor
     public Server() {
         super("SuperAwesomeNetworkProject");
+
+        myPublicKey = myEncryptor.getPublicKey();
+        myPrivateKey = myEncryptor.getPrivateKey();
+
         name = JOptionPane.showInputDialog("Enter your screen name: ");
         userText = new JTextField();
         userText.setEditable(false);
@@ -77,6 +88,7 @@ public class Server extends JFrame {
         output = new ObjectOutputStream(connection.getOutputStream());//lets you send things to the other person
         output.flush();//makes sure the output stream is clear after sending.
         input = new ObjectInputStream(connection.getInputStream());//Allows you to recieve messeges.
+        exchangeKeys();
         showMessage("\n Streams are now setup. You can begin your conversation now.");
     }
 
@@ -88,7 +100,7 @@ public class Server extends JFrame {
 
         do{//this is where the magic happens
             try{
-                payload = (String)input.readObject();//force the
+                payload = myEncryptor.getDecryptedMessage((byte[])input.readObject());//force the
                 showMessage("\n" + payload);//Display Message on a new line
             }catch (ClassNotFoundException classNotFoundException){
                 showMessage("\n Stop trying to break this with odd characters.");
@@ -109,9 +121,9 @@ public class Server extends JFrame {
     }
 
     private void sendMessage(String payload){//sends message to the client
-        //Add a second paremeter to say who sent the payload.
+        //Add a second parameter to say who sent the payload.
         try{
-            output.writeObject(name + " - " + payload);
+            output.writeObject(myEncryptor.encryptString(name + " - " + payload, clientKey));
             output.flush();
             showMessage("\n"+name+" -"+payload);
         }catch(IOException ioException){
@@ -131,6 +143,20 @@ public class Server extends JFrame {
         SwingUtilities.invokeLater(//uses a thread to add a single line of code the end of the chatwindow
                 () -> userText.setEditable(bool)
         );
+    }
+
+    private void exchangeKeys() throws IOException{
+        Object o;
+        try {
+            o = input.readObject();
+        }
+        catch (ClassNotFoundException e) {
+            return;
+        }
+        if (o instanceof PublicKey) {
+            clientKey = (PublicKey)o;
+        }
+        output.writeObject(myPublicKey);
     }
 
 }//end of class
