bfb1e9ce3c05c199f98f19d413e236211a71e9f7
nguyenmi19 <nguyenmi19@up.edu>
2018-04-25 08:18:00 -0700

    some minor changes to images

--- src/Server.java
+++ src/Server.java
@@ -89,7 +89,7 @@ public class Server extends JFrame {
                             ImageIcon icon = new ImageIcon(img);
 //                            ChatWindow.insertIcon(icon);
                             sendIcon(icon);
-                            
+
 
 
                         }
